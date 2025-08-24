ce22a1fe62880d14a67325a9d979541ba0b18191
yunjeongmi1004 <love_j90@naver.com>
2025-08-24 16:39:44 +0900

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './app'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>,
 )
